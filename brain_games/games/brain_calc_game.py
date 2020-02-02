30b0a02460f3fb72558ba831ee685f44139f4098
ashpb <ashpb@me.com>
2020-02-02 18:16:51 +1000

    Add brain-calc game

---
+++ brain_games/games/brain_calc_game.py
@@ -0,0 +1,21 @@
+from random import randint, choice
+from brain_games import game_flow
+
+GAME_DESCRIPTION = 'What is the result of the expression?'
+
+
+def generate_qa_pair():
+    number1 = randint(0, 99)
+    number2 = randint(0, 99)
+    operation = choice(['+', '-', '*'])
+    expression = "{} {} {}".format(number1, operation, number2)
+    calc_funcs = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y}
+    result = str(calc_funcs.get(operation)(number1, number2))
+    return (expression, result)
+
+
+def run():
+    game_flow.run(GAME_DESCRIPTION, generate_qa_pair)
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,9 @@
+from brain_games.games import brain_calc_game
+
+
+def main():
+    brain_calc_game.run()
+
+
+if __name__ == '__main__':
+    main()
