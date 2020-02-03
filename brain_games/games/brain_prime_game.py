07c209241b95eee902d180477d4b6d7c114af969
ashpb <ashpb@me.com>
2020-02-04 00:17:28 +1000

    Add brain-prime game

---
+++ brain_games/games/brain_prime_game.py
@@ -0,0 +1,23 @@
+from random import randint
+from math import sqrt, ceil
+from brain_games import game_flow
+
+GAME_DESCRIPTION = ('Answer "yes" if given number is prime. '
+                    'Otherwise answer "no".')
+
+
+def is_prime(number):
+    for i in range(2, ceil(sqrt(number)) + 1):
+        if (number % i) == 0:
+            return False
+    return True
+
+
+def generate_qa_pair():
+    number = randint(2, 1000)
+    result = 'yes' if is_prime(number) else 'no'
+    return (number, result)
+
+
+def run():
+    game_flow.run(GAME_DESCRIPTION, generate_qa_pair)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,9 @@
+from brain_games.games import brain_prime_game
+
+
+def main():
+    brain_prime_game.run()
+
+
+if __name__ == '__main__':
+    main()
