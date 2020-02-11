af0a492837c836a535bd11a4805dcc24634fca56
ashpb <ashpb@me.com>
2020-02-11 12:13:31 +1000

    Refactor calc game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,4 +1,5 @@
 from random import randint, choice
+from operator import add, sub, mul
 from brain_games import game_flow
 
 GAME_DESCRIPTION = 'What is the result of the expression?'
@@ -7,13 +8,9 @@ GAME_DESCRIPTION = 'What is the result of the expression?'
 def generate_qa_pair():
     number1 = randint(0, 100)
     number2 = randint(0, 100)
-    operation = choice(['+', '-', '*'])
-    expression = "{} {} {}".format(number1, operation, number2)
-    calc_funcs = {
-        '+': lambda x, y: x + y,
-        '-': lambda x, y: x - y,
-        '*': lambda x, y: x * y}
-    result = str(calc_funcs.get(operation)(number1, number2))
+    operation = choice([('+', add), ('-', sub), ('*', mul)])
+    expression = "{} {} {}".format(number1, operation[0], number2)
+    result = str(operation[1](number1, number2))
     return (expression, result)
 
 
