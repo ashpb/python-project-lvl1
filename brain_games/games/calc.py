825484ab9cda7ae2535250f82e606829f7d0cdf8
ashpb <ashpb@me.com>
2020-02-15 03:29:30 +1000

    Minor code cleanup

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,12 +2,13 @@ from random import randint, choice
 from operator import add, sub, mul
 
 DESCRIPTION = 'What is the result of the expression?'
+OPERATIONS = [('+', add), ('-', sub), ('*', mul)]
 
 
 def generate_qa_pair():
     number1 = randint(0, 100)
     number2 = randint(0, 100)
-    operation = choice([('+', add), ('-', sub), ('*', mul)])
-    expression = "{} {} {}".format(number1, operation[0], number2)
-    result = str(operation[1](number1, number2))
+    symbol, operation = choice(OPERATIONS)
+    expression = "{} {} {}".format(number1, symbol, number2)
+    result = str(operation(number1, number2))
     return (expression, result)
