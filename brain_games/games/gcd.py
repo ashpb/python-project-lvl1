1462c15758f68b48bd1c1b49f77d2bc1691462f1
ashpb <ashpb@me.com>
2020-02-15 03:33:21 +1000

    Minor stylistic change

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,10 +4,10 @@ DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
 def gcd(number1, number2):
-    if(number2 == 0):
+    if number2 == 0:
         return abs(number1)
     remainder = number1 % number2
-    if(remainder == 0):
+    if remainder == 0:
         return abs(number2)
     return gcd(number2, remainder)
 
